1e26c5bbae82b6c767a3f326a9b9b263cac96044
Yttehs <Yttehs-HDX@github.com>
2024-01-23 10:31:10 +0800

    Update version code.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "2.1.0"
+        versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
